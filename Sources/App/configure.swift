0de1a7bd24eda351b067eb38d2bedb1eebd67cd9
Sandro Kolly <kolly@ubique.ch>
2021-04-28 21:45:48 +0200

    dA: Adds database to configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,7 @@
 import Vapor
 import Leaf
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -9,6 +11,16 @@ public func configure(_ app: Application) throws {
     app.leaf.tags[IsEmptyTag.name] = IsEmptyTag()
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    let dbURL: String?
+    dbURL = Environment.get("DATABASE_URL")
+    guard let databaseURL = dbURL , var postgresConfig = PostgresConfiguration(url: databaseURL) else { fatalError() }
+    postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+    app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    app.migrations.add(SessionRecord.migration)
+
+    app.sessions.use(.fluent(.psql))
+    app.middleware.use(app.sessions.middleware)
 
     // register routes
     try routes(app)
