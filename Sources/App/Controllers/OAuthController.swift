47b1e383db5de94544d4873f13d3a96b4bf2c218
Sandro Kolly <kolly@ubique.ch>
2021-04-28 20:27:51 +0200

    dA: Adds OAuth basic flow

---
+++ Sources/App/Controllers/OAuthController.swift
@@ -0,0 +1,224 @@
+import Vapor
+import CommonCrypto
+
+private struct RequestOAuthTokenInput {
+    let consumerKey: String
+    let consumerSecret: String
+}
+
+struct RequestOAuthTokenResponse: Content {
+    let oauthToken: String
+    let oauthTokenSecret: String
+    let oauthCallbackConfirmed: String
+}
+
+struct RequestOAuthAuthenticationResponse: Content {
+    let oauth_token: String
+    let oauth_verifier: String
+}
+
+struct RequestAccessTokenInput {
+    let consumerKey: String
+    let consumerSecret: String
+    let requestToken: String // = RequestOAuthTokenResponse.oauthToken
+    let requestTokenSecret: String // = RequestOAuthTokenResponse.oauthTokenSecret
+    let oauthVerifier: String
+}
+
+struct RequestAccessTokenResponse {
+    let accessToken: String
+    let accessTokenSecret: String
+    let userId: String
+    let screenName: String
+}
+
+struct OAuthController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        routes.get("requestLogin") { req -> EventLoopFuture<Response> in
+            let input = RequestOAuthTokenInput(consumerKey: "PnnpLbBUlAa3p7VHnom0JHzgL",
+                                               consumerSecret: "vQTpmXCOxZY2b4cHBOExHsp5KuSIak0aE2uXMaLfiR2tmDngDb")
+
+            let requestTokenELF = try requestOAuthToken(req: req, args: input)
+            return requestTokenELF.map { requestToken in
+                // save token in db
+                req.redirect(to: "https://api.twitter.com/oauth/authenticate?oauth_token=\(requestToken.oauthToken)", type: .normal)
+            }
+        }
+
+        routes.get("oauthCallback") { req -> EventLoopFuture<String> in
+            let authRes = try req.query.decode(RequestOAuthAuthenticationResponse.self)
+            //db: get pw
+            // TODO: Remove dummy
+            let auth_token_secret = "abc"
+            let input = RequestAccessTokenInput(consumerKey: "PnnpLbBUlAa3p7VHnom0JHzgL",
+                                                consumerSecret: "vQTpmXCOxZY2b4cHBOExHsp5KuSIak0aE2uXMaLfiR2tmDngDb",
+                                                requestToken: authRes.oauth_token,
+                                                requestTokenSecret: auth_token_secret,
+                                                oauthVerifier: authRes.oauth_verifier)
+            let accessTokenELF = try requestAccessToken(req: req, args: input)
+            return accessTokenELF.map { accessToken in
+                return "Dein Twitter handle ist \(accessToken.screenName)"
+            }
+        }
+    }
+
+    // MARK: - request methods
+
+    private func requestOAuthToken(req: Request, args: RequestOAuthTokenInput) throws -> EventLoopFuture<RequestOAuthTokenResponse> {
+
+        let request = (url: "https://api.twitter.com/oauth/request_token", httpMethod: "POST")
+        let callback = "https://localhost:8080/oauthCallback"
+
+        var params: [String: Any] = [
+            "oauth_callback" : callback,
+            "oauth_consumer_key" : args.consumerKey,
+            "oauth_nonce" : UUID().uuidString, // nonce can be any 32-bit string made up of random ASCII values
+            "oauth_signature_method" : "HMAC-SHA1",
+            "oauth_timestamp" : String(Int(Date().timeIntervalSince1970)),
+            "oauth_version" : "1.0"
+        ]
+        // Build the OAuth Signature from Parameters
+        params["oauth_signature"] = oauthSignature(httpMethod: request.httpMethod, url: request.url,
+                                                   params: params, consumerSecret: args.consumerSecret)
+
+        // Once OAuth Signature is included in our parameters, build the authorization header
+        let authHeader = authorizationHeader(params: params)
+        let url = URI(string: request.url)
+
+        return req.client.post(url) { req in
+            req.headers.add(name: "Authorization", value: authHeader)
+        }.flatMapThrowing { res in
+            guard let body = res.body, let string = body.getString(at: body.readerIndex, length: body.capacity)
+            else { throw HTTPClientError.cancelled }
+
+            let attributes = string.urlQueryStringParameters
+
+            guard let token = attributes["oauth_token"],
+                  let tokenSecret = attributes["oauth_token_secret"],
+                  let callbackConfirmed = attributes["oauth_callback_confirmed"]
+            else { throw HTTPClientError.cancelled }
+
+            return RequestOAuthTokenResponse(oauthToken: token,
+                                             oauthTokenSecret: tokenSecret,
+                                             oauthCallbackConfirmed: callbackConfirmed)
+        }
+    }
+
+
+    private func requestAccessToken(req: Request, args: RequestAccessTokenInput) throws -> EventLoopFuture<RequestAccessTokenResponse> {
+
+      let request = (url: "https://api.twitter.com/oauth/access_token", httpMethod: "POST")
+
+      var params: [String: Any] = [
+        "oauth_token" : args.requestToken,
+        "oauth_verifier" : args.oauthVerifier,
+        "oauth_consumer_key" : args.consumerKey,
+        "oauth_nonce" : UUID().uuidString, // nonce can be any 32-bit string made up of random ASCII values
+        "oauth_signature_method" : "HMAC-SHA1",
+        "oauth_timestamp" : String(Int(Date().timeIntervalSince1970)),
+        "oauth_version" : "1.0"
+      ]
+
+      // Build the OAuth Signature from Parameters
+      params["oauth_signature"] = oauthSignature(httpMethod: request.httpMethod, url: request.url,
+                                                 params: params, consumerSecret: args.consumerSecret,
+                                                 oauthTokenSecret: args.requestTokenSecret)
+
+      // Once OAuth Signature is included in our parameters, build the authorization header
+        let authHeader = authorizationHeader(params: params)
+        let url = URI(string: request.url)
+
+        return req.client.post(url) { req in
+            req.headers.add(name: "Authorization", value: authHeader)
+        }.flatMapThrowing { res in
+            guard let body = res.body, let string = body.getString(at: body.readerIndex, length: body.capacity)
+            else { throw HTTPClientError.cancelled }
+
+            let attributes = string.urlQueryStringParameters
+
+            guard let accessToken = attributes["oauth_token"],
+                  let accessTokenSecret = attributes["oauth_token_secret"],
+                  let userId = attributes["user_id"],
+                  let screenName = attributes["screen_name"]
+            else { throw HTTPClientError.cancelled }
+
+            return RequestAccessTokenResponse(accessToken: accessToken,
+                                              accessTokenSecret: accessTokenSecret,
+                                              userId: userId,
+                                              screenName: screenName)
+        }
+    }
+
+    // MARK: - Helper methods
+    private func authorizationHeader(params: [String: Any]) -> String {
+        var parts: [String] = []
+        for param in params {
+            let key = param.key.urlEncoded
+            let val = "\(param.value)".urlEncoded
+            parts.append("\(key)=\"\(val)\"")
+        }
+        return "OAuth " + parts.sorted().joined(separator: ", ")
+    }
+
+    private func signatureKey(_ consumerSecret: String,_ oauthTokenSecret: String?) -> String {
+
+        guard let oauthSecret = oauthTokenSecret?.urlEncoded
+        else { return consumerSecret.urlEncoded+"&" }
+
+        return consumerSecret.urlEncoded+"&"+oauthSecret
+    }
+
+    private func signatureParameterString(params: [String: Any]) -> String {
+        var result: [String] = []
+        for param in params {
+            let key = param.key.urlEncoded
+            let val = "\(param.value)".urlEncoded
+            result.append("\(key)=\(val)")
+        }
+        return result.sorted().joined(separator: "&")
+    }
+
+    private func signatureBaseString(_ httpMethod: String = "POST",_ url: String, _ params: [String:Any]) -> String {
+
+        let parameterString = signatureParameterString(params: params)
+        return httpMethod + "&" + url.urlEncoded + "&" + parameterString.urlEncoded
+
+    }
+
+    private func hmac_sha1(signingKey: String, signatureBase: String) -> String {
+        // HMAC-SHA1 hashing algorithm returned as a base64 encoded string
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+        CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA1), signingKey, signingKey.count, signatureBase, signatureBase.count, &digest)
+        let data = Data(digest)
+        return data.base64EncodedString()
+    }
+
+    private func oauthSignature(httpMethod: String = "POST", url: String, params: [String: Any], consumerSecret: String, oauthTokenSecret: String? = nil) -> String {
+        let signingKey = signatureKey(consumerSecret, oauthTokenSecret)
+        let signatureBase = signatureBaseString(httpMethod, url, params)
+        return hmac_sha1(signingKey: signingKey, signatureBase: signatureBase)
+    }
+}
+
+extension String {
+    var urlEncoded: String {
+        var charset: CharacterSet = .urlQueryAllowed
+        charset.remove(charactersIn: "\n:#/?@!$&'()*+,;=")
+        return self.addingPercentEncoding(withAllowedCharacters: charset)!
+    }
+
+    var urlQueryStringParameters: Dictionary<String, String> {
+        // breaks apart query string into a dictionary of values
+        var params = [String: String]()
+        let items = self.split(separator: "&")
+        for item in items {
+            let combo = item.split(separator: "=")
+            if combo.count == 2 {
+                let key = "\(combo[0])"
+                let val = "\(combo[1])"
+                params[key] = val
+            }
+        }
+        return params
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,4 +9,5 @@ func routes(_ app: Application) throws {
     try app.register(collection: KnockOutController(mdc: mdc))
     try app.register(collection: SingleUserStatisticsController(mdc: mdc))
     try app.register(collection: MatchdayStatisticsController(mdc: mdc))
+    try app.register(collection: OAuthController())
 }
