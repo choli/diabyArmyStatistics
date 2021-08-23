d27cc51159791dbb2264ddbb0e84a03a61fba91d
Sandro Kolly <kolly@ubique.ch>
2021-08-23 03:34:10 +0200

    dA: Adds MD2

--- Parser/MyXMLParser.swift
+++ Parser/MyXMLParser.swift
@@ -17,7 +17,7 @@ public class MyXMLParser: NSObject, XMLParserDelegate {
     private var currentStep: PlayerStep = .none
 
     // MARK: - Put proper matchday in here <---- 👈🏽 🐸
-    private var completeMatchday = Spieltag(spieltag: 1)
+    private var completeMatchday = Spieltag(spieltag: 2)
 
     private enum PlayerStep: Hashable {
         case none
