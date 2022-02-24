98306a2969373deebc8e29eff0daa75b4abc14dd
choli <choli88@hotmail.com>
2022-02-24 09:15:41 +0100

    Adds MD23

--- Parser/MyXMLParser.swift
+++ Parser/MyXMLParser.swift
@@ -17,7 +17,7 @@ public class MyXMLParser: NSObject, XMLParserDelegate {
     private var currentStep: PlayerStep = .none
 
     // MARK: - Put proper matchday in here <---- 👈🏽 🐸
-    private var completeMatchday = Spieltag(spieltag: 22)
+    private var completeMatchday = Spieltag(spieltag: 23)
 
     private enum PlayerStep: Hashable {
         case none
